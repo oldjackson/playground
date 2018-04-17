e213dc6839fe2bf59e3e6c38a5f5efeeef1be7c0
Lorenzo Fermi <Lorenzofermi@gmail.com>
2018-04-18 00:16:55 +0200

    Level 3 working

---
+++ drivy/jobs/backend/level3/main.rb
@@ -0,0 +1,30 @@
+################################
+require 'json'
+require_relative 'car'
+require_relative 'rental'
+################################
+INPUT_PATH = 'data/input.json'
+OUTPUT_PATH = 'data/output.json'
+################################
+
+
+data = JSON.parse(File.read(INPUT_PATH), :symbolize_names => true)
+
+cars_data = data[:cars]
+rentals_data = data[:rentals]
+
+rentals_prices = rentals_data.map do |rent|
+  car = Car.new(cars_data.find { |c| c[:id] == rent[:car_id] })
+  rental = Rental.new({ car: car, start_date: rent[:start_date], end_date: rent[:end_date], distance: rent[:distance] })
+  {
+    id: rent[:id],
+    price: rental.price_and_commissions[:price],
+    commission: rental.price_and_commissions[:commission]
+  }
+end
+
+output = { rentals: rentals_prices }
+
+File.open(OUTPUT_PATH, 'wb') do |file|
+  file.write(JSON.pretty_generate(output))
+end
