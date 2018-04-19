c9c0468d20573b78c46fafaea1da9b83f0e6fe01
Lorenzo Fermi <Lorenzofermi@gmail.com>
2018-04-20 01:01:41 +0200

    Level 3 TDD-ed

--- drivy/jobs/backend/level3/main.rb
+++ drivy/jobs/backend/level3/main.rb
@@ -7,15 +7,14 @@ INPUT_PATH = 'data/input.json'
 OUTPUT_PATH = 'data/output.json'
 ################################
 
-
-data = JSON.parse(File.read(INPUT_PATH), :symbolize_names => true)
+data = JSON.parse(File.read(INPUT_PATH), symbolize_names: true)
 
 cars_data = data[:cars]
 rentals_data = data[:rentals]
 
 rentals_prices = rentals_data.map do |rent|
   car = Car.new(cars_data.find { |c| c[:id] == rent[:car_id] })
-  rental = Rental.new({ car: car, start_date: rent[:start_date], end_date: rent[:end_date], distance: rent[:distance] })
+  rental = Rental.new(car, rent)
   {
     id: rent[:id],
     price: rental.price_and_commissions[:price],
--- drivy/jobs/backend/level3/rental.rb
+++ drivy/jobs/backend/level3/rental.rb
@@ -1,42 +1,34 @@
 require 'date'
 ################################
-DAYS_THRESHOLDS = [1,4,10]
-THRESH_DISCOUNTS = [10,30,50]
+DAYS_THRESHOLDS = [1, 4, 10]
+THRESH_DISCOUNTS = [10, 30, 50]
 COMMISSION_PC = 30
 ROAD_ASSIST_PER_KM = 100
 ################################
 
 class Rental
-  def initialize(parameters)
-    @car = parameters[:car]
-    @start_date_str = parameters[:start_date]
-    @end_date_str = parameters[:end_date]
-    @distance = parameters[:distance]
+  def initialize(car, rental_parameters)
+    @car = car
+    @start_date = Date.parse(rental_parameters[:start_date])
+    @end_date = Date.parse(rental_parameters[:end_date])
+    raise ArgumentError.new, "The end date cannot precede the start date" if @start_date > @end_date
+    @distance = rental_parameters[:distance]
+    raise ArgumentError.new, "The distance cannot be negative" if @distance < 0
   end
 
   def price_and_commissions
-    days = rental_days()
+    days = (@end_date - @start_date + 1).to_i
     price = time_price(days) + @car.price_per_km * @distance
     commission = COMMISSION_PC * price / 100
-    assistance_fee = ROAD_ASSIST_PER_KM * days
+    assist_fee = ROAD_ASSIST_PER_KM * days
 
     {
       price: price,
-      commission: {
-        insurance_fee: commission / 2,
-        assistance_fee: assistance_fee,
-        drivy_fee: commission / 2 - assistance_fee
-      }
+      commission: { insurance_fee: commission / 2, assistance_fee: assist_fee, drivy_fee: commission / 2 - assist_fee }
     }
   end
 
   private
-
-  def rental_days
-    start_date = Date.parse(@start_date_str)
-    end_date = Date.parse(@end_date_str)
-    (end_date - start_date + 1).to_i
-  end
 
   def time_price(days)
     padded_threshs = [0] + DAYS_THRESHOLDS + [Float::INFINITY]
@@ -44,7 +36,8 @@ class Rental
 
     disc_days = 0
     floored_discs.each_index do |i|
-      disc_days += (100 - floored_discs[i]) * ( [padded_threshs[i + 1], [days, padded_threshs[i]].max ].min - padded_threshs[i] )
+      disc_days +=
+        (100 - floored_discs[i]) * ([padded_threshs[i + 1], [days, padded_threshs[i]].max].min - padded_threshs[i])
     end
     @car.price_per_day * disc_days / 100
   end
